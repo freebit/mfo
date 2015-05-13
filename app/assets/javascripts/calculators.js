cec159145976dba32f0dd1bb04b4c54049bb3b01
Freebit <freebit77@inbox.com>
2015-05-13 12:55:14 +0300

    в калькуляторе значение процентных ставок отображается с учетом 10% маржи мфо

--- app/assets/javascripts/calculators.js
+++ app/assets/javascripts/calculators.js
@@ -2,6 +2,8 @@
 
     'use strict';
 
+    var mfo_margin = 10;
+
     //выставляем тариф по умолчанию
     if ($('#order_tarif_name').length) {
         setTarif();
@@ -40,6 +42,10 @@
 
         var rate = parseFloat( $(this).val(), 10) || 0,
             mfo_rate = window.currentTarif.rate,
+            agent_rate = rate - mfo_rate,
+            mfo_margin_rate = (agent_rate / 100) * mfo_margin,
+            mfo_rate = mfo_rate + mfo_margin_rate,
+            agent_rate = agent_rate - mfo_margin_rate,
             summa = parseFloat($('#service_order_summa').val(), 10) || 0;
 
 
@@ -47,9 +53,12 @@
             alert('Укажите сумму займа')
         }else{
             //выставляем новую ставку агента
-            $('.tarif .value.agent_rate').text((rate - mfo_rate).toPrecision(3) + "%");
+            $('.tarif .value.agent_rate').text((agent_rate).toPrecision(3) + "%");
 
-
+            //если не отрицательное число, выставляем новую ставку мфо
+            if(agent_rate >= 0) {
+                $('.tarif .value.mfo_rate').text((mfo_rate).toPrecision(3) + "%");
+            }
             calcTender(summa, rate);
         }
 
@@ -88,27 +97,29 @@
             }
         }
 
-        //выставляем ставку банка
-        $('.tarif .value.mfo_rate').text(mfo_rate + "%");
-
         //считаем ставку агента
         if(base_rate > mfo_rate){
             agent_rate = base_rate - mfo_rate;
         }
-        $('.tarif .value.agent_rate').text(agent_rate + "%");
+
+        var mfo_margin_rate = (agent_rate / 100) * mfo_margin;
+
+        //выставляем ставку банка
+        $('.tarif .value.mfo_rate').text((mfo_rate + mfo_margin_rate) + "%");
+
+
+        $('.tarif .value.agent_rate').text((agent_rate - mfo_margin_rate) + "%");
     }
 
 
     function calcTender(summa, base_rate){
         var mfo_rate = window.currentTarif.rate,
-            mfo_margin = 10,
             dop_rate = window.currentTarif.dop_rate,
             agent_rate = base_rate - mfo_rate,
+            mfo_margin_rate = (agent_rate / 100) * mfo_margin,
             order_summa = (summa / 100) * base_rate,
-            agent_summa = (summa / 100) * agent_rate,
-            mfo_summa = (summa / 100) * mfo_rate,
-            full_mfo_summa = mfo_summa + ((agent_summa/100) * mfo_margin),
-            full_agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
+            agent_summa = (summa / 100) * (agent_rate - mfo_margin_rate),
+            mfo_summa = (summa / 100) * (mfo_rate + mfo_margin_rate);
 
             //если это тариф типа Б, где при победе снимается еще одна ставка
             if(dop_rate > 0){
@@ -118,26 +129,26 @@
                     dop_mfo_summa = (summa / 100) * dop_mfo_rate,
                     dop_agent_summa = (summa / 100) * dop_agent_rate;
 
-                order_summa += dop_summa
-                full_mfo_summa += dop_mfo_summa;
-                full_agent_summa += dop_agent_summa;
+                order_summa += dop_summa;
+                mfo_summa += dop_mfo_summa;
+                agent_summa += dop_agent_summa;
              }
 
         //сумма МФО не должна быть меньше указанного минимума
-        full_mfo_summa = full_mfo_summa < window.currentTarif.minimum ? window.currentTarif.minimum : full_mfo_summa
+        mfo_summa = mfo_summa < window.currentTarif.minimum ? window.currentTarif.minimum : mfo_summa
 
         //выставляем значения в калькуляторе
         $('#service_dogovor_summa').val(order_summa);
-        $('#service_mfo_summa').val(full_mfo_summa);
-        $('#service_agent_summa').val(full_agent_summa);
+        $('#service_mfo_summa').val(mfo_summa);
+        $('#service_agent_summa').val(agent_summa);
 
         //выставляем значения в заявке
         $('#order_tarif').val( $('#order_tarif_name').val() );
         $('#order_base_rate').val(base_rate);
         $('#order_summa').val(summa);
         $('#order_dogovor_summa').val(order_summa);
-        $('#order_mfo_summa').val(full_mfo_summa);
-        $('#order_agent_summa').val(full_agent_summa);
+        $('#order_mfo_summa').val(mfo_summa);
+        $('#order_agent_summa').val(agent_summa);
 
     }
 
