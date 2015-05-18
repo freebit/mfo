16e535a7f04f8a4b92dd1888d2415e911a77dee3
Freebit <freebit77@inbox.com>
2015-05-18 21:03:29 +0300

    поправлена работа калькулятора при минималке

--- app/assets/javascripts/calculators.js
+++ app/assets/javascripts/calculators.js
@@ -15,7 +15,7 @@
                 base_rate = parseFloat($('#service_order_rate').val(), 10) || 0;
 
 
-            calcTender(summa, base_rate);
+            Calculate(summa, base_rate);
         }
 
         //считаем при смене платформы или типа тарифа
@@ -30,7 +30,7 @@
             if (summa <= 0) {
                 alert('Укажите сумму займа')
             } else {
-                calcTender(summa, base_rate);
+                Calculate(summa, base_rate);
             }
         });
 
@@ -41,24 +41,12 @@
             // if((e.keyCode >= 37 && e.keyCode <= 40) || e.keyCode == 37 || e.keyCode == 8) return;
 
             var rate = parseFloat( $(this).val(), 10 ) || 0,
-                mfo_rate = window.currentTarif.rate,
-                agent_rate = rate - mfo_rate,
-                mfo_margin_rate = 0,
-                summa = parseFloat($('#service_order_summa').val(), 10) || 0,
-                dogovor_summa = parseFloat($('#service_dogovor_summa').val(), 10) || 0;
-
-            if(dogovor_summa > window.currentTarif.minimum && rate > 0){
-                mfo_margin_rate = (agent_rate / 100) * mfo_margin;
-                mfo_rate = mfo_rate + mfo_margin_rate;
-                agent_rate = agent_rate - mfo_margin_rate;
-            }
-
+                summa = parseFloat($('#service_order_summa').val(), 10) || 0;
 
             if(summa <= 0){
                 alert('Укажите сумму займа')
             }else{
-                setRates(mfo_rate, agent_rate);
-                calcTender(summa, rate);
+                Calculate(summa, rate);
             }
 
         });
@@ -73,95 +61,96 @@
             if (summa <= 0) {
                 alert('Укажите сумму займа')
             } else {
-                calcTender(summa, rate);
+                Calculate(summa, rate);
             }
+        });
+
+        //считаем при смене дохода агента
+        $('#service_agent_summa').on('keyup, change', function (e) {
+            alert($(this).val())
         });
 
 
         function setTarif(platform, type) {
             var platform = platform || $('#order_platform_name').val(),
-                type = type || $('#order_tarif_name').val(),
-                base_rate = parseInt($('#service_order_rate').val(), 10) || 0,
-                summa = parseFloat($('#service_order_summa').val(), 10) || 0,
-                order_summa = (summa / 100) * base_rate,
-                mfo_rate = 0,
-                agent_rate = 0;
+                type = type || $('#order_tarif_name').val();
 
             for (var i = 0, ln = window.tarifs.length; i < ln; i++) {
                 if (window.tarifs[i].platform == platform && window.tarifs[i].type_t == type) {
                     window.currentTarif = window.tarifs[i];
-                    mfo_rate = window.currentTarif.rate;
                     break;
                 }
             }
-
-            //считаем ставку агента
-            if (base_rate > mfo_rate) {
-                agent_rate = base_rate - mfo_rate;
-            }
-
-
-            if(order_summa > window.currentTarif.minimum) {
-                var mfo_margin_rate = (agent_rate / 100) * mfo_margin;
-                mfo_rate = mfo_rate + mfo_margin_rate;
-                agent_rate = agent_rate - mfo_margin_rate;
-            }
-
-            //выставляем ставку банка
-            setRates(mfo_rate, agent_rate);
-
-
-
         }
 
 
-        function calcTender(summa, base_rate) {
+        function Calculate(summa, base_rate) {
             var mfo_rate = window.currentTarif.rate,
-                dop_rate = window.currentTarif.dop_rate,
                 agent_rate = base_rate - mfo_rate,
-                mfo_margin_rate = (agent_rate / 100) * mfo_margin,
+
+                dohod_summa_tarif = (summa / 100) * window.currentTarif.rate, //посчитаем доход по тарифу
+                minimalka = dohod_summa_tarif <= window.currentTarif.minimum,
+
                 order_summa = (summa / 100) * base_rate,
                 order_summa = order_summa > window.currentTarif.minimum ? order_summa : window.currentTarif.minimum,
                 agent_summa = 0,
                 mfo_summa = window.currentTarif.minimum,
-                mfo_summa_without_margin = 0,
-                mfo_margin_value = 0,
                 dop_summa = 0,
+                mfo_forminimal_rate = 0,
+                agent_forminimal_rate = 0,
+
+                dop_rate = window.currentTarif.dop_rate,
                 dop_mfo_rate = 1.5,
                 dop_agent_rate = 0.5,
                 dop_mfo_summa = 0,
                 dop_agent_summa = 0;
 
+                if(minimalka){
+                    agent_summa = order_summa - mfo_summa;
+                    mfo_summa = mfo_summa + ((agent_summa/100) * mfo_margin);
+                    agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
 
+                    mfo_forminimal_rate = (mfo_summa/summa) * 100;
+                    agent_forminimal_rate = (agent_summa/summa) * 100;
 
-                if(order_summa > window.currentTarif.minimum) {
-                    agent_summa = (summa / 100) * (agent_rate - mfo_margin_rate);
-                    mfo_summa_without_margin = (summa / 100) * mfo_rate;
-                    mfo_summa = (summa / 100) * (mfo_rate + mfo_margin_rate);
-                    mfo_margin_value = (agent_summa / 100) * mfo_margin;
+                    setRates(mfo_forminimal_rate, agent_forminimal_rate);
 
-                    //сумма МФО не должна быть меньше указанного минимума
-                    mfo_summa = mfo_summa_without_margin < window.currentTarif.minimum ? (window.currentTarif.minimum + mfo_margin_value) : mfo_summa;
+                }else{
+                    agent_summa = (summa / 100) * agent_rate;
+                    mfo_summa = ((summa / 100) * mfo_rate) + ((agent_summa/100) * mfo_margin);
+                    agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
 
-                    //если это тариф типа Б, где при победе снимается еще одна ставка
-                    if (dop_rate) {
-                        dop_summa = (summa / 100) * dop_rate;
-                        dop_mfo_summa = (summa / 100) * dop_mfo_rate;
-                        dop_agent_summa = (summa / 100) * dop_agent_rate;
-
-                        order_summa += dop_summa;
-                        mfo_summa += dop_mfo_summa;
-                        agent_summa += dop_agent_summa;
-                    }
+                    var mfo_margin_rate = (agent_rate / 100) * mfo_margin;
+                    mfo_rate = mfo_rate + mfo_margin_rate;
+                    agent_rate = agent_rate - mfo_margin_rate;
+                    setRates(mfo_rate, agent_rate);
                 }
 
-            //посчитаем доход договора по тарифу
-            var dogovor_summa_tarif = (summa / 100) * window.currentTarif.rate;
+            //если это тариф типа Б, где при победе снимается еще одна ставка
+            if(dop_rate) {
+                dop_summa = (summa / 100) * dop_rate;
+                dop_mfo_summa = (summa / 100) * dop_mfo_rate;
+                dop_agent_summa = (summa / 100) * dop_agent_rate;
 
+                order_summa += dop_summa;
+                mfo_summa += dop_mfo_summa;
+                agent_summa += dop_agent_summa;
+
+                if(minimalka){
+                    mfo_forminimal_rate += dop_mfo_rate;
+                    agent_forminimal_rate += dop_agent_rate;
+                    setRates(mfo_forminimal_rate, agent_forminimal_rate);
+                }else{
+                    mfo_rate += dop_mfo_rate;
+                    agent_rate += dop_agent_rate;
+                    setRates(mfo_rate, agent_rate);
+                }
+
+            }
 
 
             //выставляем значения в калькуляторе
-            $('#service_dogovor_summa_tarif').val(dogovor_summa_tarif);
+            $('#service_dogovor_summa_tarif').val(dohod_summa_tarif);
             $('#service_dogovor_summa').val(order_summa);
             $('#service_dogovor_summa').val(order_summa);
             $('#service_mfo_summa').val(mfo_summa);
