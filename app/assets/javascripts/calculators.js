4009bf10819b5c18cce5e51411f262cc1ea24eaf
Freebit <freebit77@inbox.com>
2015-05-26 21:43:11 +0300

    добавлен пересчет калькулятора по смене дохода договора и дохода агента

--- app/assets/javascripts/calculators.js
+++ app/assets/javascripts/calculators.js
@@ -48,13 +48,13 @@
             if(summa <= 0){
                 alert('Укажите сумму займа')
             }else{
-                Calculate(summa, rate);
+                Calculate(summa, rate, true);
             }
 
         });
 
 
-        //считаем при смене суммы
+        //считаем при смене суммы займа
         $('#service_order_summa').on('keyup, change', function (e) {
             var rate = parseFloat($('#service_order_rate').val(), 10) || 0,
                 summa = parseFloat($(this).val(), 10) || 0;
@@ -67,9 +67,45 @@
             }
         });
 
+
+        //считаем при смене дохода договора
+        $('#service_dogovor_summa').on('keyup, change', function(e){
+            var summa = parseFloat($('#service_order_summa').val(), 10) || 0;
+            if(summa == 0) return;
+
+            var order_summa = parseFloat($(this).val(), 10),
+                order_rate = (order_summa/summa) * 100;
+
+            $('#service_order_rate').val(order_rate)
+
+            Calculate(summa, order_rate, true);
+
+
+        });
+
         //считаем при смене дохода агента
         $('#service_agent_summa').on('keyup, change', function (e) {
-            //alert($(this).val())
+
+            var summa = parseFloat($('#service_order_summa').val(), 10) || 0;
+
+            if(summa == 0) return;
+
+            var agent_summa = parseFloat($(this).val(), 10);
+
+
+            //пересчитаем доход МФО
+            var mfo_summa = ((summa/100) * window.currentTarif.rate) + ((agent_summa/100) * mfo_margin),
+                order_summa = floorFigure(mfo_summa + agent_summa, 2),
+                order_rate = floorFigure((order_summa/summa) * 100, 2),
+                mfo_rate_fact = floorFigure((mfo_summa/summa) * 100, 2),
+                agent_rate = floorFigure(agent_summa * (100/summa), 2);
+
+            $('#service_mfo_summa').val( floorFigure(mfo_summa, 2) );
+            $('#service_dogovor_summa').val(order_summa);
+            $('#service_order_rate').val(order_rate);
+
+            //сменим ставки
+            setRates(mfo_rate_fact, agent_rate);
         });
 
 
@@ -92,17 +128,18 @@
         }
 
 
-        function Calculate(summa, base_rate) {
-            var mfo_rate = window.currentTarif.rate,
+        function Calculate(summa, base_rate, by_rate_flag) {
+            var by_rate = !!by_rate_flag,
+                mfo_rate = window.currentTarif.rate,
+                minimum = floorFigure(window.currentTarif.minimum, 2),
                 agent_rate = base_rate - mfo_rate,
 
-                dohod_summa_tarif = (summa / 100) * window.currentTarif.rate, //посчитаем доход по тарифу
-                minimalka = dohod_summa_tarif <= window.currentTarif.minimum,
+                dohod_summa_tarif = floorFigure(((summa / 100) * window.currentTarif.rate), 2), //посчитаем доход по тарифу
+                minimalka = dohod_summa_tarif <= minimum,
 
-                order_summa = (summa / 100) * base_rate,
-                order_summa = order_summa > window.currentTarif.minimum ? order_summa : window.currentTarif.minimum,
+                order_summa = floorFigure(((summa / 100) * base_rate), 2),
                 agent_summa = 0,
-                mfo_summa = window.currentTarif.minimum,
+                mfo_summa = 0,
                 dop_summa = 0,
                 mfo_forminimal_rate = 0,
                 agent_forminimal_rate = 0,
@@ -116,25 +153,43 @@
                 mfo_victory_summa = 0,
                 agent_victory_summa = 0;
 
+                //преобразуем по минималке
+                if(summa > 0) {
+
+                    order_summa = order_summa > minimum ? order_summa : minimum;
+                    mfo_summa = minimum;
+                }
+
                 if(minimalka){
-                    agent_summa = order_summa - mfo_summa;
-                    mfo_summa = mfo_summa + ((agent_summa/100) * mfo_margin);
-                    agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
+                    if(summa > 0) {
+                        agent_summa = order_summa - mfo_summa;
+                        mfo_summa = mfo_summa + ((agent_summa/100) * mfo_margin);
+                        mfo_summa = floorFigure(mfo_summa, 2);
+                        agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
+                        agent_summa = floorFigure(agent_summa, 2);
 
-                    mfo_forminimal_rate = (mfo_summa/summa) * 100;
-                    agent_forminimal_rate = (agent_summa/summa) * 100;
+                        mfo_forminimal_rate = floorFigure((mfo_summa / summa) * 100, 2);
+                        agent_forminimal_rate = floorFigure((agent_summa / summa) * 100, 2);
 
-                    setRates(mfo_forminimal_rate.toFixed(1), agent_forminimal_rate.toFixed(1));
+                        mfo_rate = floorFigure(((window.currentTarif.minimum * 100) / summa), 2);
+                    }
+
+                    setRates(mfo_forminimal_rate, agent_forminimal_rate);
+
 
                 }else{
-                    agent_summa = (summa / 100) * agent_rate;
-                    mfo_summa = ((summa / 100) * mfo_rate) + ((agent_summa/100) * mfo_margin);
-                    agent_summa = agent_summa - ((agent_summa/100) * mfo_margin);
+                    if(summa > 0) {
+                        agent_summa = (summa / 100) * agent_rate;
+                        mfo_summa = ((summa / 100) * mfo_rate) + ((agent_summa / 100) * mfo_margin);
+                        agent_summa = agent_summa - ((agent_summa / 100) * mfo_margin);
 
-                    var mfo_margin_rate = (agent_rate / 100) * mfo_margin;
-                    mfo_rate = mfo_rate + mfo_margin_rate;
-                    agent_rate = agent_rate - mfo_margin_rate;
-                    setRates(mfo_rate.toFixed(1), agent_rate.toFixed(1));
+                        var mfo_margin_rate = (agent_rate / 100) * mfo_margin;
+                        mfo_rate = floorFigure((mfo_rate + mfo_margin_rate), 2);
+                        agent_rate = floorFigure(agent_rate - mfo_margin_rate, 2);
+                    }
+
+                    setRates(mfo_rate, agent_rate);
+
                 }
 
             //если это тариф типа Б, где при победе снимается еще одна ставка
@@ -147,24 +202,19 @@
                 mfo_victory_summa = mfo_summa + dop_mfo_summa;
                 agent_victory_summa = agent_summa + dop_agent_summa;
 
-                //if(minimalka){
-                //    mfo_forminimal_rate += dop_mfo_rate;
-                //    agent_forminimal_rate += dop_agent_rate;
-                //    setRates(mfo_forminimal_rate.toFixed(2), agent_forminimal_rate.toFixed(2));
-                //}else{
-                //    mfo_rate += dop_mfo_rate;
-                //    agent_rate += dop_agent_rate;
-                //    setRates(mfo_rate.toFixed(1), agent_rate.toFixed(1));
-                //}
 
             }
 
 
             //выставляем значения в калькуляторе
             $('#service_dogovor_summa_tarif').val(dohod_summa_tarif);
+
             $('#service_dogovor_summa').val(order_summa);
-            $('#service_order_rate').val(mfo_rate);
+
+            !by_rate && $('#service_order_rate').val(mfo_rate);
+
             $('#service_mfo_summa').val(mfo_summa);
+
             $('#service_agent_summa').val(agent_summa);
 
             //при победе тарифа Б
@@ -186,18 +236,11 @@
             $('.tarif .value.agent_rate').text((agent_rate || 0) + "%");
         }
 
-
-
-        function twoFixed(number){
-            var strNumber = "";
-            try{
-                strNumber = new String(number);
-            }catch(err){
-                console.log(err);
-            }
-
-
-        }
+        function floorFigure(figure, decimals){
+            if (!decimals) decimals = 2;
+            var d = Math.pow(10,decimals);
+            return parseFloat((parseInt(figure*d)/d).toFixed(decimals), 10);
+        };
 
 
 
